0fce41ce70c9029190041a2594c5e479aef348be
dlangbe <David.Langbehn@amd.com>
2023-06-07 11:40:00 -0700

    Updated f32 bilinear tree & corrected output

--- library/src/contraction/contraction_selection.cpp
+++ library/src/contraction/contraction_selection.cpp
@@ -140,7 +140,6 @@ namespace hiptensor
         CHECK_HIP_ALLOC(hipFree(wspace));
 
         *winnerMetrics = bestMetrics;
-
         *winner = bestSolution;
 
         if(bestSolution == nullptr)
@@ -199,7 +198,323 @@ namespace hiptensor
                          std::vector<std::size_t> const&                         e_ms_ns_strides,
                          const uint64_t                                          workspaceSize)
         {
-            return HIPTENSOR_STATUS_SUCCESS;
+            int d1 = a_ms_ks_lengths[0];
+            int d2 = a_ms_ks_lengths[1];
+            int d3 = b_ns_ks_lengths[0];
+            int d4 = b_ns_ks_lengths[1];
+            int d5 = a_ms_ks_lengths[2];
+            int d6 = a_ms_ks_lengths[3];
+
+            size_t unique_id = 0;
+
+            if(d6 <= 28)
+            {
+                if(d6 <= 8)
+                {
+                    unique_id = 4781938049531404654;
+                }
+                else
+                {
+                    if(d5 <= 32)
+                    {
+                        if(d5 <= 24)
+                        {
+                            unique_id = 4781938049531404654;
+                        }
+                        else
+                        {
+                            if(d6 <= 17)
+                            {
+                                unique_id = 4781938049531404654;
+                            }
+                            else
+                            {
+                                if(d2 <= 369)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 16334338346691940719;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(d4 <= 406)
+                        {
+                            if(d5 <= 52)
+                            {
+                                if(d6 <= 16)
+                                {
+                                    unique_id = 4781938049531404654;
+                                }
+                                else
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                            }
+                            else
+                            {
+                                unique_id = 5897150915348629714;
+                            }
+                        }
+                        else
+                        {
+                            if(d5 <= 56)
+                            {
+                                if(d6 <= 16)
+                                {
+                                    unique_id = 4781938049531404654;
+                                }
+                                else
+                                {
+                                    unique_id = 16334338346691940719;
+                                }
+                            }
+                            else
+                            {
+                                if(d1 <= 457)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(d5 <= 12)
+                {
+                    if(d5 <= 8)
+                    {
+                        if(d3 <= 491)
+                        {
+                            unique_id = 4781938049531404654;
+                        }
+                        else
+                        {
+                            unique_id = 4781938049531404654;
+                        }
+                    }
+                    else
+                    {
+                        if(d6 <= 56)
+                        {
+                            if(d3 <= 493)
+                            {
+                                unique_id = 4781938049531404654;
+                            }
+                            else
+                            {
+                                if(d1 <= 282)
+                                {
+                                    unique_id = 4781938049531404654;
+                                }
+                                else
+                                {
+                                    unique_id = 16334338346691940719;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(d2 <= 274)
+                            {
+                                unique_id = 5897150915348629714;
+                            }
+                            else
+                            {
+                                if(d4 <= 299)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 16334338346691940719;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(d6 <= 56)
+                    {
+                        if(d5 <= 50)
+                        {
+                            if(d2 <= 362)
+                            {
+                                unique_id = 5897150915348629714;
+                            }
+                            else
+                            {
+                                if(d4 <= 320)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 16334338346691940719;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(d4 <= 401)
+                            {
+                                if(d6 <= 46)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                            else
+                            {
+                                if(d2 <= 77)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(d2 <= 302)
+                        {
+                            if(d5 <= 46)
+                            {
+                                if(d1 <= 457)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                            else
+                            {
+                                if(d1 <= 115)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(d1 <= 43)
+                            {
+                                if(d1 <= 19)
+                                {
+                                    unique_id = 5897150915348629714;
+                                }
+                                else
+                                {
+                                    unique_id = 10972102817010133142;
+                                }
+                            }
+                            else
+                            {
+                                unique_id = 10972102817010133142;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if(auto candidate = candidates.find(unique_id); candidate != candidates.end())
+            {
+                // Make sure that we calculate full element space incase strides are not packed.
+                auto sizeA = elementSpaceFromLengthsAndStrides(a_ms_ks_lengths, a_ms_ks_strides)
+                            * hipDataTypeSize(typeA);
+                auto sizeB = elementSpaceFromLengthsAndStrides(b_ns_ks_lengths, b_ns_ks_strides)
+                            * hipDataTypeSize(typeB);
+                auto sizeD = 0;
+                if(typeD != NONE_TYPE)
+                {
+                    sizeD = elementSpaceFromLengthsAndStrides(d_ms_ns_lengths, d_ms_ns_strides)
+                            * hipDataTypeSize(typeD);
+                }
+                auto sizeE = elementSpaceFromLengthsAndStrides(e_ms_ns_lengths, e_ms_ns_strides)
+                            * hipDataTypeSize(typeE);
+
+                void *A_d, *B_d, *D_d, *E_d, *wspace;
+                float alpha = 1.02f;
+                float beta  = 1.03f;
+
+                CHECK_HIP_ALLOC(hipMalloc(&A_d, sizeA));
+                CHECK_HIP_ALLOC(hipMalloc(&B_d, sizeB));
+                CHECK_HIP_ALLOC(hipMalloc(&D_d, sizeD));
+                CHECK_HIP_ALLOC(hipMalloc(&E_d, sizeE));
+                CHECK_HIP_ALLOC(hipMalloc(&wspace, workspaceSize));
+
+                auto* solution = candidate->second;
+
+                if(solution->initArgs(&alpha,
+                                  A_d,
+                                  B_d,
+                                  &beta,
+                                  D_d,
+                                  E_d,
+                                  a_ms_ks_lengths,
+                                  a_ms_ks_strides,
+                                  b_ns_ks_lengths,
+                                  b_ns_ks_strides,
+                                  d_ms_ns_lengths,
+                                  d_ms_ns_strides,
+                                  e_ms_ns_lengths,
+                                  e_ms_ns_strides,
+                                  wspace)
+                && solution->workspaceSize() <= workspaceSize)
+                {   
+                    auto    time = (*solution)(StreamConfig{nullptr, true});
+                    int32_t m, n, k;
+                    std::tie(m, n, k) = solution->problemDims();
+                    auto flops        = std::size_t(2) * m * n * k;
+                    auto bytes        = solution->problemBytes();
+
+                    PerfMetrics metrics = {
+                        solution->uid(), // id
+                        solution->kernelName(), // name
+                        time, // avg time
+                        static_cast<float>(flops) / static_cast<float>(1.E9) / time, // tflops
+                        static_cast<float>(bytes) / static_cast<float>(1.E6) / time // BW
+                    };
+
+                    *winner = solution;
+                    *winnerMetrics = metrics;
+                }   
+
+                CHECK_HIP_ALLOC(hipFree(A_d));
+                CHECK_HIP_ALLOC(hipFree(B_d));
+                CHECK_HIP_ALLOC(hipFree(D_d));
+                CHECK_HIP_ALLOC(hipFree(E_d));
+                CHECK_HIP_ALLOC(hipFree(wspace));
+
+                return HIPTENSOR_STATUS_SUCCESS;
+            }
+            else
+            {
+                return HIPTENSOR_STATUS_EXECUTION_FAILED;
+            }
         }
     };
 
